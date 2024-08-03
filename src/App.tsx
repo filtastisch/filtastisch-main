5296bfc35b36aae5854fe8efb3f857949814f0e7
fil <filtastisch@gmail.com>
2024-08-03 11:12:33 +0200

    tests

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Test from './Test'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/test" element={<Test />} />
+    </Routes>
+    </BrowserRouter>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -15,6 +22,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -24,6 +32,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <a href="/test">TEST</a>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
